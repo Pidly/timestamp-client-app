2484ddbff4428840ed82bbe36b8d3ab2af0d0c6b
unknown <ji773rs@gmail.com>
2022-11-23 19:35:23 -0800

    Added full URL for the fetch

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react';
 
 function GetData() {
-    
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch("https://timestamp-server-production.up.railway.app/home")
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
+
     return (
         <div>
             <p>Data Info</p>
-            
+            <p>{data.unix}</p>
+            <p>{data.date}</p>
         </div>
     );
 }
