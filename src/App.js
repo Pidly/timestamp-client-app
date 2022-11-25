681391830a86f5af64f06d37c3900a041efdedfc
pidly <pidly@devins-mbp.lan>
2022-11-24 21:43:35 -0800

    Added some styling and another api fetch

--- src/App.js
+++ src/App.js
@@ -4,21 +4,10 @@ import GetData from './components/GetData';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <GetData />
-      </header>
+      
+      
+      <GetData />
+      
     </div>
   );
 }
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,20 +1,42 @@
 import { useState, useEffect } from 'react';
 
 function GetData() {
-    const [data, setData] = useState({})
-    const baseUri = "https://timestamp-server-production.up.railway.app";
+    const [data, setData] = useState({});
+    const [epochData, setEpochData] = useState({});
+    //app.get("/epoch", (req, res) => {
+
+    const localUrl = "http://localhost:3001";
+    const serverUrl = "https://timestamp-server-production.up.railway.app";
+    const baseUri = localUrl;
 
     useEffect(() => {
         fetch(baseUri + "/home")
         .then(res => res.json())
         .then(data => setData(data))
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        console.log("Epoch use effect");
+        fetch(baseUri + "/epoch")
+        .then(res => res.json())
+        .then(epochData => setEpochData(epochData))
+    }, []);
 
     return (
         <div>
-            <p>Data Info</p>
-            <p>{data.unix}</p>
-            <p>{data.date}</p>
+            <>
+                <h2>Input</h2>
+                <p>1453708800</p>
+                <p>2015-12-25</p>
+            </>
+            <div>
+                <h2>Output Date</h2>
+                <p>{data.unix}</p>
+                <p>{data.date}</p>
+                <h2>Output Epoch</h2>
+                <p>{epochData.unix}</p>
+                <p>{epochData.date}</p>
+            </div>
         </div>
     );
 }
